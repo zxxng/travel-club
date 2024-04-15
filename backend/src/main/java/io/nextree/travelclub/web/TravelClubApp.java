e4193281308c68c6f6c5c4b1a42acfb75191b094
zxxng <jeayeong123@naver.com>
2024-04-15 21:42:04 +0900

    rename: Main -> TravelClubApp 클래스명 변경

---
+++ backend/src/main/java/io/nextree/travelclub/web/TravelClubApp.java
@@ -0,0 +1,11 @@
+package io.nextree.travelclub.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelClubApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelClubApp.class);
+    }
+}
