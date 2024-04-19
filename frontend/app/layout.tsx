d32fb073fdbcf39408bd7dd02f4635802e493d4b
zxxng <jeayeong123@naver.com>
2024-04-19 16:39:32 +0900

    dependencies(fron): radix-ui/themes 추가

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,8 @@ import { cn } from '@/util/utils';
 import { Plane } from 'lucide-react';
 import logo from '@/public/nextree_logo.svg';
 import Image from 'next/image';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,19 +25,21 @@ export default function RootLayout({
       <body
         className={cn('w-screen min-h-screen bg-primary-blue', inter.className)}
       >
-        <div className="flex flex-col max-w-[1220px] min-h-screen p-10 bg-white mx-auto text-center">
-          <header className="flex gap-1 justify-center items-center mx-auto mb-6">
-            <Plane
-              className="w-9 h-9"
-              // style={{ color: 'var(--primary-blue)' }}
-            />
-            <h1 className="text-3xl font-bold">Travel Club</h1>
-          </header>
-          <main className="grow bg-gray-300">{children}</main>
-          <footer className="w-full">
-            <Image src={logo} alt="logo" className="w-16 mx-auto mt-4" />
-          </footer>
-        </div>
+        <Theme>
+          <div className="flex flex-col max-w-[1220px] min-h-screen p-10 bg-white mx-auto text-center">
+            <header className="flex gap-1 justify-center items-center mx-auto mb-6">
+              <Plane
+                className="w-9 h-9"
+                // style={{ color: 'var(--primary-blue)' }}
+              />
+              <h1 className="text-3xl font-bold">Travel Club</h1>
+            </header>
+            <main className="grow bg-gray-300">{children}</main>
+            <footer className="w-full">
+              <Image src={logo} alt="logo" className="w-16 mx-auto mt-4" />
+            </footer>
+          </div>
+        </Theme>
       </body>
     </html>
   );
